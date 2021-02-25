025600e92d472f115d12fc3efafb05106086a572
coolkid2021 <71680629+coolkid2021@users.noreply.github.com>
2021-02-24 20:38:44 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,97 +1,41 @@
-var Engine = Matter.Engine,
-    World = Matter.World,
-    Events = Matter.Events,
-    Bodies = Matter.Bodies; 
-var particles = [];
-var plinkos = [];
-var divisions =[];
-
-var divisionHeight=300;
-var score = 0;
-var count = 0;
-var gameState ="start";
+var dog, happyDog, database
+var foodS, foodStock;
+function preload()
+{
+  dog = loadImage("../images/dogImg.png");
+  happyDog = loadImage("../images/donImg1.png");
+}
 
 function setup() {
-  createCanvas(800, 800);
-  engine = Engine.create();
-  world = engine.world;
-  ground = new Ground(width/2,height,width,20);
-
-   for (var k = 0; k <=width; k = k + 80) {
-     divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
-   }
-    for (var j = 75; j <=width; j=j+50) {
-       plinkos.push(new Plinko(j,75));
-    }
-
-    for (var j = 50; j <=width-10; j=j+50) {
-        plinkos.push(new Plinko(j,175));
-    }
-
-    for (var j = 75; j <=width; j=j+50) {
-        plinkos.push(new Plinko(j,275));
-    }
-
-    for (var j = 50; j <=width-10; j=j+50) {
-        plinkos.push(new Plinko(j,375));
-    }
-    
+  createCanvas(500, 500);
+  dog = createSprite(250,250);
+  dog.addImage("dog",dog);
+  happyDog.addImage("Happy Dog", happyDog)
+  database = firebase.database();
+  foodStock = database.ref('Food');
+    foodStock.on("value", readStock)
 }
- 
-function draw() {
-  background("black");
-  textSize(35)
-  text("Score : "+score,20,40);
-  fill("white");
-  textSize(35)
-  text(" 500 ", 5, 550);
-  text(" 500 ", 80, 550);
-  text(" 500 ", 160, 550);
-  text(" 500 ", 240, 550);
-  text(" 100 ", 320, 550);
-  text(" 100 ", 400, 550);
-  text(" 100 ", 480, 550);
-  text(" 200 ", 560, 550);
-  text(" 200 ", 640, 550);
-  text(" 200 ", 720, 550);
-  Engine.update(engine);
-  ground.display();
-  
-  if ( count>= 5) {
-    gameState ="end";
-    textSize(100);
-    text("GameOver", 150, 250);
+
+
+function draw() {  
+  background(46, 139, 87)
+  dog.display();
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS)
+    dog.addImage(dogHappy)
   }
+  drawSprites();
+  //add styles here
 
-  for (var i = 0; i < plinkos.length; i++) {
-     plinkos[i].display();  
-  }
- 
-  for (var i = 0; i < particles.length; i++) {
-     particles[i].display();
-      
-     if (particles[i].body.position.x < 300 && particles[i].body.position.y>760) {
-      score=score+500;
-      particles.pop();
-     }
-    else if (particles[i].body.position.x < 600 && particles[i].body.position.x > 301 && particles[i].body.position.y > 760) {
-      score = score + 100;
-      particles.pop();
-    }
-    else if (particles[i].body.position.x < 900 && particles[i].body.position.x > 601 && particles[i].body.position.y > 760) {
-      score = score + 200;
-      particles.pop();
-    }
-   }
+}
+function readStock(data){
+  foodS = data.val();
+}
+function writeStock(x){
 
-   for (var k = 0; k < divisions.length; k++) {
-     divisions[k].display();
-   }
- 
+  database.ref('/').update({
+    Food:x
+  })
 }
-function mousePressed(){
-  if(gameState!=="end"){
-      count++;
-     particles.push(new Particle(mouseX, 10, 10, 10)); 
-  }   
-}
+
+
